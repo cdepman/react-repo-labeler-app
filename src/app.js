06727f0e6ba3355e27f0aa1fa7207260876c9bcb
Charlie Depman <cdepman@gmail.com>
2016-01-10 21:47:14 -0800

    first react render

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
-document.write('Hello World!')
+var React = require('react');
 
+var Hello = React.createClass({
+  // render method returns the state
+  render: function () {
+    return <div>Hello, {this.props.name}!</div>
+  }
+});
+
+React.render(<Hello name="Charlie"/>, document.body);
